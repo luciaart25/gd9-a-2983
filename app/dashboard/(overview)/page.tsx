1be1b8a0d3d4923d18250db1b241d2bcd033d2cb
luciaart25 <luciaartanti894@gmail.com>
2026-05-10 07:47:44 +0700

    Fix dashboard rendering for Vercel

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -11,6 +11,8 @@ import {
   CardsSkeleton,
 } from '@/app/ui/skeletons';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
 
